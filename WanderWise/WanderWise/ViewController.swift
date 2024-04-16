3b59a60a94515a3d9031c150aab344f346a15ea3
geenalimfat <glimfat@ucsd.edu>
2024-04-15 23:03:52 -0700

    Added X-Code Project

---
+++ WanderWise/WanderWise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WanderWise
+//
+//  Created by Geena Limfat on 4/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
